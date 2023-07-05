503701d2d49fd03e68b65358a78bed541ffa0ba1
Luca Koenig <56564513+Acul08@users.noreply.github.com>
2023-07-06 08:52:00 +0900

    attempts on sync audio and video

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import time
-
 import cv2
 import numpy as np
 import pyaudio
 import wave
+import moviepy.editor as mp
 
 def record():
+
+
     # Set the number of channels, sample rate, and recording duration
     num_channels = 16
-    sample_rate = 44100
-    duration = 20 # in seconds
+    sample_rate = 48000
+    duration = 5  # in seconds
 
     # Initialize PyAudio
     audio = pyaudio.PyAudio()
@@ -19,7 +21,7 @@ def record():
                         channels=num_channels,
                         rate=sample_rate,
                         input=True,
-                        frames_per_buffer=1024)
+                        frames_per_buffer=2048)
 
     # Initialize video recording
     width = 640
@@ -32,12 +34,19 @@ def record():
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
     # Start recording
-    frames = []
     audio_frames = []
-    num_audio_frames = int(duration * sample_rate / 1024)
+    num_audio_frames = int(duration * sample_rate / 2048)
+    start_time = time.time()
     for i in range(num_audio_frames):
+        # Synchronize audio and video streams
+        current_time = time.time() - start_time
+        expected_frame_time = i * 2048 / sample_rate
+        delay = expected_frame_time - current_time
+        if delay > 0:
+            time.sleep(delay)
+
         # Capture audio frame
-        data = stream.read(1024)
+        data = stream.read(2048)
         audio_frames.append(data)
 
         # Capture video frame
@@ -45,7 +54,6 @@ def record():
         if ret:
             frame = cv2.resize(frame, (width, height))
             video_out.write(frame)
-            frames.append(frame)
 
     # Stop recording and close the audio and video streams
     stream.stop_stream()
@@ -64,11 +72,23 @@ def record():
 
     # Check the length of the audio and video data arrays
     print("Length of audio data:", len(audio_frames))
-    print("Length of video data:", len(frames))
 
     # Check the actual frame rate of the video capture device
     actual_fps = cap.get(cv2.CAP_PROP_FPS)
     print("Actual frame rate:", actual_fps)
+
+
+    # # Load the audio and video files
+    # audio = mp.AudioFileClip("recording.wav")
+    # video = mp.VideoFileClip("recording.mp4")
+    #
+    # # Synchronize the audio and video streams
+    # synced_audio = audio.set_start(video.start)
+    # synced_video = video.set_audio(synced_audio)
+    #
+    # # Write the synchronized video to a file
+    # synced_video.write_videofile("synced_video.mp4", fps=video.fps)
+
 def recordAudio():
 
     # Set the number of channels, sample rate, and recording duration
@@ -110,4 +130,11 @@ def recordAudio():
 if __name__ == '__main__':
     record()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+# wip command for capture
+# ffmpeg -f dshow -i video="Logi C270 HD WebCam" -f dshow -i audio="Line (MCHStreamer Multi-channels)" -map 0:v -map 1:a -c:v libx264 -c:a aac -b:a 384k -ac 16 test.mp4
+
+
