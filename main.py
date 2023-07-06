f016de8a7de8787d76778e4cfeaaf679294b33d3
Luca Koenig <56564513+Acul08@users.noreply.github.com>
2023-07-06 13:44:43 +0900

    added synchronization with clock

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ import moviepy.editor as mp
 
 def record():
 
-
     # Set the number of channels, sample rate, and recording duration
     num_channels = 16
     sample_rate = 48000
-    duration = 5  # in seconds
+    duration = 20  # in seconds
 
     # Initialize PyAudio
     audio = pyaudio.PyAudio()
@@ -21,12 +20,12 @@ def record():
                         channels=num_channels,
                         rate=sample_rate,
                         input=True,
-                        frames_per_buffer=2048)
+                        frames_per_buffer=1024)
 
     # Initialize video recording
     width = 640
     height = 480
-    fps = 30
+    fps = 25
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     video_out = cv2.VideoWriter('recording.mp4', fourcc, fps, (width, height))
 
@@ -35,24 +34,24 @@ def record():
 
     # Start recording
     audio_frames = []
-    num_audio_frames = int(duration * sample_rate / 2048)
-    start_time = time.time()
-    for i in range(num_audio_frames):
-        # Synchronize audio and video streams
-        current_time = time.time() - start_time
-        expected_frame_time = i * 2048 / sample_rate
-        delay = expected_frame_time - current_time
+    video_frames = []
+    start_time = time.monotonic()
+    for i in range(int(duration * fps)):
+        # Synchronize audio and video streams using a common clock
+        expected_frame_time = start_time + i / fps
+        delay = expected_frame_time - time.monotonic()
         if delay > 0:
             time.sleep(delay)
 
         # Capture audio frame
-        data = stream.read(2048)
+        data = stream.read(1024)
         audio_frames.append(data)
 
         # Capture video frame
         ret, frame = cap.read()
         if ret:
             frame = cv2.resize(frame, (width, height))
+            video_frames.append(frame)
             video_out.write(frame)
 
     # Stop recording and close the audio and video streams
@@ -72,22 +71,16 @@ def record():
 
     # Check the length of the audio and video data arrays
     print("Length of audio data:", len(audio_frames))
+    print("Length of video data:", len(video_frames))
 
-    # Check the actual frame rate of the video capture device
-    actual_fps = cap.get(cv2.CAP_PROP_FPS)
-    print("Actual frame rate:", actual_fps)
+    # Use moviepy to combine the audio and video streams into a single file with synchronized timing
+    audio_clip = mp.AudioFileClip("recording.wav")
+    video_clip = mp.ImageSequenceClip(video_frames, fps=fps)
+    final_clip = video_clip.set_audio(audio_clip)
+    final_clip.write_videofile("synced_video.mp4", fps=fps)
 
 
-    # # Load the audio and video files
-    # audio = mp.AudioFileClip("recording.wav")
-    # video = mp.VideoFileClip("recording.mp4")
-    #
-    # # Synchronize the audio and video streams
-    # synced_audio = audio.set_start(video.start)
-    # synced_video = video.set_audio(synced_audio)
-    #
-    # # Write the synchronized video to a file
-    # synced_video.write_videofile("synced_video.mp4", fps=video.fps)
+
 
 def recordAudio():
 
@@ -136,5 +129,7 @@ if __name__ == '__main__':
 
 # wip command for capture
 # ffmpeg -f dshow -i video="Logi C270 HD WebCam" -f dshow -i audio="Line (MCHStreamer Multi-channels)" -map 0:v -map 1:a -c:v libx264 -c:a aac -b:a 384k -ac 16 test.mp4
+# ffmpeg -f dshow -r 25 -i video="Logi C270 HD WebCam" -use_wallclock_as_timestamps 1 -f dshow -i audio="Line (MCHStreamer Multi-channels)" -map 0:v -map 1:a -b:a 384k -ac 16 -async 0 -max_muxing_queue_size 1024 test.mp4
+
 
 
