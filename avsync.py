dea34e3153ad6ea1afb172b1ef09284ee8985641
Luca Koenig <56564513+Acul08@users.noreply.github.com>
2023-07-07 12:35:07 +0900

    changed the line it's reading in the sync file

--- avsync.py
+++ avsync.py
@@ -5,22 +5,22 @@ import os
 
 
 def combine_vid_and_audio(wavfile, vidfile, syncfile, outvidfile, vid_fps=30, audio_fs=44100, cam_delay=0.0):
-    
+
     tmp_vidfile = './tmp.mp4'
 
     T = np.genfromtxt(syncfile, delimiter=',', skip_header=1)
-    T = T[:, [0, 2]]
+    T = T[:, [0, 3]]
 
     T[:,1] = T[:,1]/audio_fs
     t = np.arange(0, T[-10,1], 1/vid_fps)
     n_frame = t.shape[0]
 
     vr = cv2.VideoCapture(vidfile)
-    fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v') 
-    vw = cv2.VideoWriter(tmp_vidfile, fmt, vid_fps, (int(vr.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vr.get(cv2.CAP_PROP_FRAME_HEIGHT))), isColor=True) 
+    fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    vw = cv2.VideoWriter(tmp_vidfile, fmt, vid_fps, (int(vr.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vr.get(cv2.CAP_PROP_FRAME_HEIGHT))), isColor=True)
 
     crnt_ts = -10
-    cnt = 0  
+    cnt = 0
     for i_frame in tqdm(range(n_frame)):
         while crnt_ts+cam_delay < t[i_frame]:
             ret, frame = vr.read()
