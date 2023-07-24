ce71294094e5ddaffe9429deac185e5631195ada
Luca Koenig <56564513+Acul08@users.noreply.github.com>
2023-07-24 16:55:37 +0900

    automated camera calibration and made file name modification function to simplify code

--- main.py
+++ main.py
@@ -3,12 +3,18 @@
 
 
 import shutil
+
+import h5py
+import cv2
 import avsync
 import os
 import datetime
 import recording
 import usvcam_main.usvcam.analysis as analysis
+import misc.camera_calib.camera_calib as cameracalib
 import calibration
+from PIL import Image, ImageTk
+from tkinter import Tk, Label
 import pyaudio
 
 # Audio settings
@@ -60,20 +66,13 @@ def record_audio_trigger(data_path):
 
     audio_recording_out_path = []
     for i in range(len(mic_array_devices)):
-        if i == 0:
-            audio_recording_out_path.append(os.path.join(recording_dir, audio_recording_out_filename))
-        else:
-            audio_recording_out_path.append(f"{os.path.join(recording_dir, audio_recording_out_filename)[:-4]}_{i}.wav")
-
+        audio_recording_out_path.append(os.path.join(recording_dir, modify_file_name(audio_recording_out_filename, i)))
         recording.rearrange_wav_channels(audio_recording_out_path[i], mic_array_layout[i],
                                          audio_recording_out_path[i])
 
     video_recording_out_path = []
     for i in range(len(camera_devices)):
-        if i == 0:
-            video_recording_out_path.append(os.path.join(recording_dir, video_recording_out_filename))
-        else:
-            video_recording_out_path.append(f"{os.path.join(recording_dir, video_recording_out_filename)[:-4]}_{i}.mp4")
+        video_recording_out_path.append(os.path.join(recording_dir, modify_file_name(video_recording_out_filename, i)))
 
     # USV segmentation
     input("Stop Video Recording and press enter. File gets read from temp folder")
@@ -81,37 +80,39 @@ def record_audio_trigger(data_path):
     video_recording_out_path = []
     video_recording_temp_path = []
     for i in range(len(camera_devices)):
-        if i == 0:
-            video_recording_out_path.append(os.path.join(recording_dir, video_recording_out_filename))
-            video_recording_temp_path.append(os.path.join(temp_path, video_recording_out_filename))
-        else:
-            video_recording_out_path.append(f"{os.path.join(recording_dir, video_recording_out_filename)[:-4]}_{i}.mp4")
-            video_recording_temp_path.append(f"{os.path.join(temp_path, video_recording_out_filename)[:-4]}_{i}.mp4")
+        video_recording_out_path.append(os.path.join(recording_dir, modify_file_name(video_recording_out_path, i)))
+        video_recording_temp_path.append(os.path.join(temp_path, modify_file_name(video_recording_temp_path, i)))
 
-        while (not os.path.exists(video_recording_temp_path[i])):
-            input(f"No video file {i} found in temp folder, please move it there and check if the name is correct")
+    while (not os.path.exists(video_recording_temp_path[i])):
+        input(f"No video file {i} found in temp folder, please move it there and check if the name is correct")
 
-        if os.path.exists(video_recording_temp_path[i]):
-            shutil.move(video_recording_temp_path[i], video_recording_out_path[i])
-        else:
-            print(f"File {i} not found. Cannot move the file.")
+    if os.path.exists(video_recording_temp_path[i]):
+        shutil.move(video_recording_temp_path[i], video_recording_out_path[i])
+    else:
+        print(f"File {i} not found. Cannot move the file.")
 
     output_path = []
-    for i in range(min(len(camera_devices),len(mic_array_devices))):
-        if i == 0:
-            output_path.append(os.path.join(recording_dir, output_filename))
-        else:
-            output_path.append(f"{os.path.join(recording_dir, output_filename)[:-4]}_{i}.mp4")
+    for i in range(min(len(camera_devices), len(mic_array_devices))):
+        output_path.append(os.path.join(recording_dir, modify_file_name(output_filename, i)))
 
         avsync.combine_vid_and_audio(audio_recording_out_path[i],
                                      video_recording_out_path[i],
-                                     #TODO fix but that only outputs one frame on second file
+                                     # TODO fix but that only outputs one frame on second file
                                      os.path.join(recording_dir, syncfile_filename),
-                                     output_path[i],i, fps, sample_rate, cam_delay)
-
-
+                                     output_path[i], i, fps, sample_rate, cam_delay)
 
     return recording_dir
+
+
+def modify_file_name(file_name, i):
+    # Split the file name into name and extension parts
+    name, extension = file_name.split('.')
+    if i is 0:
+        return file_name
+    # Create the modified file name with the provided number
+    modified_file_name = f"{name}_{i}.{extension}"
+
+    return modified_file_name
 
 
 # makes analysis based on the default names. only processes first file without _i extention.
@@ -141,7 +142,83 @@ def analysis_default(recording_dir):
     analysis.create_localization_video(recording_dir, mic_calibration_path, color_eq=False)
 
 
+def calibrate_cameras(recording_dir):
+    for i in range(len(camera_devices)):
+        video_path = os.path.join(recording_dir, modify_file_name(video_recording_out_filename, i))
+        print("Start camera calibration with: " + modify_file_name(video_recording_out_filename, i))
+        cameracalib.analyze_chessboardvid(video_path, 23, saveimg=True, frame_intv=5)
+        print("Generating calibration file")
+        cameracalib.calibrate_intrinsic(video_path)
+        mtx = None
+        dist = None
+        with h5py.File(os.path.splitext(video_path)[0] + '_cam_intrinsic.h5', mode='r') as f:
+            print(f.keys())
+            mtx = f['/mtx'][()]
+            dist = f['/dist'][()]
+
+        print("CAMEAR MATRIX:")
+        print(mtx)
+
+        print("DISTORTION COEFFS:")
+        print(dist)
+        running = True
+
+        try:
+            if os.path.exists(os.path.abspath("./misc/camera_calib/tmp")):
+                # Use os.startfile to open the Windows Explorer window at the specified path
+                os.startfile(os.path.abspath("./misc/camera_calib/tmp"))
+            else:
+                print("Path does not exist.")
+        except Exception as e:
+            print("An error occurred:", e)
+
+        while running:
+            user_input = input(
+                "Put in the number of picture in Camera Calibration Temp you want to undistort as a test. \nOtherwise just press Enter ")
+            if user_input is "":
+                running = False
+            elif os.path.exists(os.path.join(os.path.abspath("./misc/camera_calib/tmp"), user_input + ".jpg")):
+                img = cv2.imread(os.path.join(os.path.abspath("./misc/camera_calib/tmp"), user_input + ".jpg"))
+                h, w = img.shape[:2]
+                newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+
+                # undistort
+                dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+
+                # crop the image
+                x, y, w, h = roi
+                dst = dst[y:y + h, x:x + w]
+                cv2.imwrite(os.path.join(os.path.abspath("./misc/camera_calib/tmp"), user_input + '_calibresult.png'), dst)
+
+                # Open the images using PIL
+                image_distorded = Image.open(os.path.join(os.path.abspath("./misc/camera_calib/tmp"), user_input + ".jpg"))
+                image_undistorded = Image.open(os.path.join(os.path.abspath("./misc/camera_calib/tmp"), user_input + '_calibresult.png'))
+                max_height = max(image_distorded.height,
+                                 image_undistorded.height)  # Find the maximum height between the two images
+                total_width = image_distorded.width + image_undistorded.width  # Calculate the total width required for both images side by side
+                combined_image = Image.new('RGB', (
+                total_width, max_height))  # Create a new blank image to combine the two images
+                combined_image.paste(image_distorded, (0, 0))  # Paste the first image on the left side
+                combined_image.paste(image_undistorded,
+                                     (image_distorded.width, 0))  # Paste the second image on the right side
+                root = Tk()  # Show the combined image using tkinter
+                root.title("Images Side by Side")
+
+                # Convert the PIL image to a PhotoImage object to display in tkinter
+                img_tk = ImageTk.PhotoImage(combined_image)
+
+                # Create a label and set the image
+                label = Label(root, image=img_tk)
+                label.image = img_tk
+                label.pack()
+
+                root.mainloop()
+            else:
+                print("File does not exist")
+
+
 if __name__ == "__main__":
     # recording.get_microphone_info()
-    recording_dir = record_audio_trigger(data_path)
+    # recording_dir = record_audio_trigger(data_path)
     # analysis_default(recording_dir)
+    calibrate_cameras(r"C:\Users\experimenter\PycharmProjects\Accustic_Camera\data\2023-07-24-14-34-47")
